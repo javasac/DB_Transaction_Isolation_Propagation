3af4bef43eaf172c42defe73064a40619b84445f
javasac <sachin5kansal@gmail.com>
2026-05-05 11:19:46 -0500

    Added properties for performance optimization.

--- src/main/java/com/sachin/CountryCityOneMany/CountryCityOneManyApplication.java
+++ src/main/java/com/sachin/CountryCityOneMany/CountryCityOneManyApplication.java
@@ -1,11 +1,12 @@
 package com.sachin.CountryCityOneMany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-// this is optional.
 @EnableTransactionManagement
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class CountryCityOneManyApplication
 {
 	public static void main(String[] args)
